db48b729dd10e1538b2b0bf9deb7345abe2985cd
EduardoE <eduardo_bikke@hotmail.com>
2017-03-31 23:42:00 -0300

    ok

--- EduardoEsterferson/src/ES201602.java
+++ EduardoEsterferson/src/ES201602.java
@@ -1,7 +1,8 @@
 
 public class ES201602 {
 	public static void main(String[] args) {
-		System.out.println("Engenharia de Software 2016.2 - Atividade prática - Eduardo Esterferson");
+		System.out.println("Engenharia de Software 2016.2 - Atividade pratica - Eduardo Esterferson");
 		System.out.println("Daniel Deyson ");
+		System.out.println("OK!!");
 	}
 }
