bd61441483653ca043fb8d120234b9629644dd68
danieldeyson <daniel.deyson@dce.ufpb.br>
2017-03-31 23:38:21 -0300

    Update ES201602.java

--- EduardoEsterferson/src/ES201602.java
+++ EduardoEsterferson/src/ES201602.java
@@ -1,6 +1,7 @@
 
 public class ES201602 {
 	public static void main(String[] args) {
-		System.out.println("Engenharia de Software 2016.2 - Atividade pr·tica - Eduardo Esterferson");
+		System.out.println("Engenharia de Software 2016.2 - Atividade pr√°tica - Eduardo Esterferson");
+		System.out.println("Daniel Deyson ");
 	}
 }
